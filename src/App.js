33c0c7837e20bb47e9216ee0ad5e93e5c1ffb0f3
Kreston <kreston.cm@gmail.com>
2021-12-28 18:07:25 -0600

    phase 1 complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import harvardArt from './data/harvardArt.js'
+import GalleryNavigation from './components/GalleryNavigation/index.js'
 
 // console.log(harvardArt)
+
+
 
 function App() {
   return (
     <div>
-      <h1>Hello From App</h1>
+      <GalleryNavigation galleries={harvardArt.records}/>
+      {/* <h1>Hello From App</h1> */}
     </div>
   );
 }
--- src/components/GalleryNavigation/index.js
+++ src/components/GalleryNavigation/index.js
@@ -1,7 +1,22 @@
-const GalleryNavigation = () => {
-    <h1></h1>setInterval(() => {
-        
-    }, dddddddd);
-    dddddddddddjjj
-    debugdddd
+import {NavLink} from 'react-router-dom'
+import './GalleryNavigation.css'
+
+
+const GalleryNavigation = ({galleries}) => {
+    console.log(galleries)
+    return (
+        <nav>
+            <h1 className='blue'>Galleries</h1>
+            <NavLink exact to={`/`}>Home</NavLink>
+            {galleries.map((gallery) => (
+                <span key={gallery.id} style={{ paddingRight: '10px' }}
+                >
+                    <NavLink to={`/galleries/${gallery.id}`}>{gallery.name}</NavLink> |
+                </span>
+            ))}
+
+        </nav>
+    )
 }
+
+export default GalleryNavigation;
